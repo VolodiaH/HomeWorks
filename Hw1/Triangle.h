b584e4128fa95d25575b2a2168478db493f850ae
Volodymyr Horodechnyi <volodymyr.horodechnyi@cdev.com.ua>
2023-06-15 20:12:56 +0300

    The first homework

---
+++ Hw1/Triangle.h
@@ -0,0 +1,57 @@
+﻿#pragma once
+#include <memory>
+
+class ITriangle 
+{
+public:
+	virtual void drawTriangle() = 0;
+};
+
+class Triangle: public ITriangle
+{
+protected:
+	Triangle(int length);
+
+	int length();
+
+private:
+	int m_length;
+};
+
+class TriangleFactory 
+{
+public:
+	static TriangleFactory& instance();
+
+	std::unique_ptr<Triangle> makeRight(int length);
+	std::unique_ptr<Triangle> makeIsosceles(int length);
+
+private:
+	TriangleFactory();
+
+	class RightTriangle: public Triangle
+	{
+	public:
+		RightTriangle(int length);
+
+	private:
+		void drawTriangle();
+
+		void drawRightTriangle();
+	};
+
+	class IsoscelesTriangle: public Triangle
+	{
+	public:
+		IsoscelesTriangle(int length);
+
+	private:
+		void drawTriangle();
+
+		void drawTop();
+		void drawBody();
+		void drawBottom();
+
+	};
+};
+
---
+++ Hw1/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include "Triangle.h"
+
+std::unique_ptr<Triangle> chooseTriangle(int length)
+{
+	std::string answer = "";
+	std::cout << "A right triangle? (y,n) ";
+	std::cin >> answer;
+	if (answer[0] == 'y')
+		return TriangleFactory::instance().makeRight(length);
+	else
+		return TriangleFactory::instance().makeIsosceles(length);
+}
+
+int main() 
+{
+	//you asked about it
+	std::cout << "I love C++\n";
+
+	constexpr int lowBoundary = 5;
+	constexpr int highBoundary = 53;
+	constexpr int maxWrongCount = 3;
+
+	int wrongEnterCount = 0;
+
+	while (true)
+	{
+		std::cout << "\nLength of triangle(" << lowBoundary << "-" << highBoundary << "): ";
+
+		int length = 5;
+		std::cin >> length;
+		if (lowBoundary <= length && length <= highBoundary)
+		{
+			std::unique_ptr<Triangle>tr = chooseTriangle(length);
+			tr->drawTriangle();
+
+			wrongEnterCount = 0;
+		}
+		else
+		{
+			++wrongEnterCount;
+			if (wrongEnterCount == maxWrongCount)
+			{
+				std::cout << "Not today!";
+				return -1;
+			}
+			
+			if (!std::cin.good())
+			{
+				std::cout << "NUMBERS PLEASE!";
+				std::cin.clear();
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			}
+			else
+				std::cout << "I know.... It is very hard - but try again";
+			
+			continue;
+		}
+		
+		std::cout << "\nDraw again?(y,n)";
+		std::string answer = "";
+		std::cin >> answer;
+
+		switch (answer[0])
+		{
+		case 'y':
+			break;
+		case 'n':
+			std::cout << "Bye!";
+			return 0;
+		default:
+			std::cout << "I will take it for NO";
+			return 0;
+			break;
+		}
+
+	}
+	return 0;
+}

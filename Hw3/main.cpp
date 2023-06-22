f17df26b1eb819623a5be4b936ad5141d82b252c
Volodymyr Horodechnyi <horodechyyv@gmail.com>
2023-06-22 21:48:07 +0300

    Third homework - operators

---
+++ Hw3/main.cpp
@@ -0,0 +1,108 @@
+
+#include <iostream>
+// next 2 headers with C++20
+#include <format>
+#include <numbers>
+
+auto main(int argc, char* argv[]) -> void 
+{
+	{
+		std::cout << "\n----------------------------------1---------------------------------------\n";
+
+		unsigned long long time;
+
+		std::cout << "Enter time: ";
+		std::cin >> time;
+
+		constexpr int secondsInMinute = 60;
+		constexpr int secondsInHour = secondsInMinute * 60;
+
+		int hours{ 0 }, minutes{ 0 };
+
+		if (time >= secondsInHour) 
+		{
+			hours = time / secondsInHour;
+			time -= hours * secondsInHour;
+		}
+		if (time >= secondsInMinute) 
+		{
+			minutes = time / secondsInMinute;
+			time -= minutes * secondsInMinute;
+		}
+		
+		std::cout << std::format("Hours: {}, Minutes: {}, Seconds: {}", hours, minutes, time);
+
+	}
+
+	{
+		std::cout << "\n----------------------------------2---------------------------------------\n";
+
+		constexpr int numbersCount = 3;
+		int first{ 0 }, second{ 0 }, third{ 0 };
+		std::cout << std::format("Enter {} numbers: ", numbersCount);
+		std::cin >> first >> second >> third;
+
+		int sum{ first + second + third };
+		int product{ first * second * third };
+		int averageArithmetic{ sum / numbersCount };
+
+		std::cout << std::format("Sum: {} \n", sum);
+		std::cout << std::format("Product: {}\n", product);
+		std::cout << std::format("Average Arithmetic: {}\n", averageArithmetic);
+
+		std::cout << std::format(" first({}) equal second({}) and  is {}\n", first, second, first == second);
+		std::cout << std::format(" second({}) equal third({}) and  is {}\n", first, second, second == third);
+
+	}
+
+	{
+		std::cout << "\n----------------------------------3---------------------------------------\n";
+		
+        int first{ 0 }, second{ 0 };
+		std::cout << "Enter 2 numbers: ";
+		std::cin >> first >> second;
+
+		bool lessThan{ first < second }, equal{ first == second }, moreThan{ first > second }, lessOrEqual{ first <= second };
+
+		std::cout << lessThan << "/" << equal << "/" << moreThan << "/" << lessOrEqual << '\n';
+
+		// appears that std::format already deduce bool as true\false not 1\0... I hope it is not UB
+		// next is for raw string std::cout... if you need it
+		//std::cout << std::boolalpha;
+		std::cout << std::format("Less Than of the first({}) and the second({}) is {}\n", first, second, lessThan);
+		std::cout << std::format("Equal of the first({}) and the second({}) is {}\n", first, second, equal);
+		std::cout << std::format("More Than of the first({}) and the second({}) is {}\n", first, second, moreThan);
+		std::cout << std::format("Less Or Equal of the first({}) and the second({}) is {}\n", first, second, lessOrEqual);
+
+	}
+
+	{
+		std::cout << "\n----------------------------------4---------------------------------------\n";
+
+		int width{ 0 }, height{ 0 };
+
+		std::cout << "Enter width:";
+		std::cin >> width;
+		std::cout << "Enter height:";
+		std::cin >> height;
+
+		int square = width * height;
+		int perimeter = (width + height) * 2;
+
+		std::cout << std::format("Square is {}, Perimeter is {}", square, perimeter);
+	}
+
+	{
+		std::cout << "\n----------------------------------5---------------------------------------\n";
+		
+		std::cout << "Enter radius: ";
+
+		double radius{ 0 };
+		std::cin >> radius;
+
+		long double square = std::numbers::pi_v<double> * (radius * radius);
+		long double length = 2 * std::numbers::pi_v<double> * radius;
+
+		std::cout << std::format("Square is {}, Length is {}", square, length);
+	}
+}

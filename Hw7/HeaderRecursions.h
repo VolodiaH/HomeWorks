8162948e5d222a26b78ccacd356f2a4077768b77
Volodymyr Horodechnyi <horodechnyyv@gmail.com>
2023-07-06 22:25:35 +0300

    Functions

---
+++ Hw7/HeaderRecursions.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace recursion
+{
+	int factorial(int number);
+	void printNaturalNumbersToBig(int count);
+	void printNaturalNumbersFromBig(int count);
+}
---
+++ Hw7/main.cpp
@@ -0,0 +1,11 @@
+﻿#include "HeaderLoops.h"
+#include "HeaderRecursions.h"
+
+int main() 
+{
+	task1(34);
+	loops::printNaturalNumbersFromBig(10);
+	loops::printNaturalNumbersToBig(10);
+	recursion::printNaturalNumbersFromBig(10);
+	recursion::printNaturalNumbersToBig(10);
+}

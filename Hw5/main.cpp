2205247b6bd800086a29dae3f29ef1d3d29f7a8a
Volodymyr Horodechnyi <horodechnyyv@gmail.com>
2023-07-06 09:13:27 +0300

    Whatever

--- Hw5/main.cpp
+++ Hw5/main.cpp
@@ -7,7 +7,7 @@ void task3();
 void task4();
 void task5();
 
-int main() 
+int main()
 {
 	task1();
 	task2();
@@ -16,7 +16,7 @@ int main()
 	task5();
 }
 
-void task1() 
+void task1()
 {
 	std::cout << "Enter init value, step, count value:";
 
@@ -30,11 +30,11 @@ void task1()
 	}
 }
 
-void task2() 
+void task2()
 {
 	std::cout << "Enter fib number: ";
-	
-	std::size_t number{ 0 };
+
+	size_t number{ 0 };
 	std::cin >> number;
 
 	int firstNumber{ 0 }, secondNumber{ 1 };
@@ -52,86 +52,93 @@ long long factorial(int number)
 {
 	long long res{ 1 };
 
-	for (int i{ number }; i > 0; --i)
+	for (int i{ number }; i > 0; --i) {
 		res *= i;
+	}
 
 	return res;
 }
 
-void task3() 
+void task3()
 {
 	std::cout << "Enter factorial number: ";
-	
+
 	int number{ 0 };
 	std::cin >> number;
 
 	std::cout << std::format("Your number: {}", factorial(number));
 }
 
-std::string stripedString(std::size_t count)
+std::string stipedString(std::size_t count)
 {
-	std::string res("");
+	std::string res;
+	res.reserve(count);
 
-	for (std::size_t i{ 0 }; i < count; ++i)
-		res.insert(res.begin(), (i & 1) ? '0' : '1');
+	for (std::size_t i{ count }; i > 0; --i) {
+		res.push_back((i & 1) ? '1' : '0');
+	}
 
 	return res;
 }
 
-void task4() 
+void task4()
 {
 	std::cout << "Enter triangle length: ";
 
-	std::size_t length{ 0 };
+	size_t length{ 0 };
 	std::cin >> length;
 
 	//a
 	{
-		for (std::size_t i{ 0 }; i < length; ++i)
+		for (std::size_t i{ 0 }; i < length; ++i) {
 			std::cout << std::string(i + 1, '*') << '\n';
+		}
 
 		std::cout << "\n";
 	}
 	//b
 	{
-		for (std::size_t i{ length }; i > 0; --i)
+		for (std::size_t i{ length }; i > 0; --i) {
 			std::cout << std::string(i, '*') << '\n';
-
+		}
 		std::cout << "\n";
 	}
 	//c
 	{
-		for (std::size_t i{ 0 }; i < length; ++i)
+		for (std::size_t i{ 0 }; i < length; ++i) {
 			std::cout << std::string(i, ' ') + std::string(length, '*') << '\n';
-
+		}
 		std::cout << "\n";
 	}
 	//d
 	{
-		for (std::size_t i{ 1 }; i <= length; ++i)
-			std::cout << stripedString(i) << '\n';
+		for (std::size_t i{ 1 }; i <= length; ++i) {
+			std::cout<<stipedString(i)<<'\n';
+		}
 	}
 }
 
-void task5() 
+void task5()
 {
 	std::cout << "Enter character: ";
 	char symbol{ 0 };
-	std::cin >> symbol; 
+	std::cin >> symbol;
 
 	int sum{ 0 };
 
 	while (symbol != '.')
 	{
-		if (std::isalpha(symbol) && std::islower(symbol))
+		if (std::isalpha(symbol) && std::islower(symbol)) {
 			std::cout << std::format("Your symbol: {}\n", static_cast<char>(std::toupper(symbol)));
+		}
 		else if (std::isdigit(symbol))
 		{
 			sum += symbol - '0';
 			std::cout << std::format("Sum: {}\n", sum);
 		}
-		else
+		else {
 			std::cout << "I do not support it!!! \n";
+		}
 
 		std::cout << "Enter character: ";
 		std::cin >> symbol;

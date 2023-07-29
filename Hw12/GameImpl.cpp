5fc80b629540085bf15fac7178af0d8b63349495
VolodiaH <136325043+VolodiaH@users.noreply.github.com>
2023-07-29 23:00:30 +0300

    Update GameImpl.cpp

--- Hw12/GameImpl.cpp
+++ Hw12/GameImpl.cpp
@@ -17,7 +17,7 @@ std::vector<SymbolMatch> GameImpl::compare2Strings(const std::string &secretWord
     return fullMatch;
 }
 
-void GameImpl::PrcoceedSymbol(std::vector<SymbolMatch> &match, const std::string &guess, char symbol) const
+void GameImpl::proceedSymbol(std::vector<SymbolMatch> &match, const std::string &guess, char symbol) const
 {
     for (size_t i{}; i < guess.length(); ++i)
     {

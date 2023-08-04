248d7e0ea63e513fd22a1def15df1e062f91b638
Volodymyr Horodechnyi <horodechnyyv@gmail.com>
2023-08-04 12:15:55 +0300

    Operators

---
+++ Hw15/Rectangle.cpp
@@ -0,0 +1,22 @@
+﻿#include "Rectangle.h"
+
+constexpr float PerimeterCoefficient = 2.0f;
+
+size_t ObjectCounter<Rectangle>::m_createdObjects = 0;
+
+Rectangle::Rectangle(const float length, const float height) :
+m_length(length),
+m_height(height)
+{
+}
+
+float Rectangle::perimeter() const
+{
+    return PerimeterCoefficient * (m_height + m_length);
+}
+
+float Rectangle::square() const
+{
+    return m_height * m_length;
+}
+

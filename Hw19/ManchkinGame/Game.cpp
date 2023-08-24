42d5c61834d1225677122eee0480da29460739c9
VolodiaH <136325043+VolodiaH@users.noreply.github.com>
2023-08-24 15:58:40 +0300

    Update Game.cpp

--- Hw19/ManchkinGame/Game.cpp
+++ Hw19/ManchkinGame/Game.cpp
@@ -219,7 +219,8 @@ void Game::run()
 				{
 					UI::printModifiers(&m_munchkin);
 					const int choice = UI::selectModifierFromHand(static_cast<short>(m_munchkin.getModifiers().size()));
-					
+
+					// 6 deep nest.....
 					if (choice != -1)
 						fight.applyModifier(choice - 1);
 				}
